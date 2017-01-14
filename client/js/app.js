f40b6507ea4a3c964fa29d5725dcd4777f80bc40
Mary <malystrasza@gmail.com>
2017-01-14 00:01:15 +0000

    About and contact controller and partials added

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,8 @@
 var myApp = angular.module('myApp', ['ngRoute']).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
-    // $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});
-    // $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'contactController'});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});
+    $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'contactController'});
     // $routeProvider.when('/projects', {templateUrl: 'partials/projects.html', controller: 'projectsController'});    
 
     // $routeProvider.when('/projects/customerapi', {templateUrl: '/partials/projects/customerapi.html', controller: 'customerApiController'});

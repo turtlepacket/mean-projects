3c081b6d6191b992732429910b3ca4181b688f7b
Mary <malystrasza@gmail.com>
2017-01-20 04:55:09 +0000

    Added carousel to home and created about page

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']).
+var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap']).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
     $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});

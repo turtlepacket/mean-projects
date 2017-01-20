a5ee19e3b7429ba7d5949701e9d3754d3b57ea9a
Mary <malystrasza@gmail.com>
2017-01-20 06:34:45 +0000

    Added Project controller and routes

--- client/js/app.js
+++ client/js/app.js
@@ -3,7 +3,7 @@ config(['$routeProvider', '$locationProvider', function($routeProvider, $locatio
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
     $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});
     $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'contactController'});
-    // $routeProvider.when('/projects', {templateUrl: 'partials/projects.html', controller: 'projectsController'});    
+    $routeProvider.when('/projects', {templateUrl: 'partials/projects.html', controller: 'projectsController'});    
 
     // $routeProvider.when('/projects/customerapi', {templateUrl: '/partials/projects/customerapi.html', controller: 'customerApiController'});
     // $routeProvider.when('/projects/chat', {templateUrl: '/partials/projects/chat.html', controller: 'chatController'});    
